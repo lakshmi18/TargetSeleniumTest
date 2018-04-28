b497ed89e3c17c47cc5de92196e16ee08559825e
lakshmi18 <lakshmi18.kv@gmail.com>
2018-04-28 13:00:28 -0500

    Updated'

---
+++ src/TRG/TargetWebPage.java
@@ -0,0 +1,35 @@
+package TRG;
+
+
+
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.*;
+
+
+
+
+
+public class TargetWebPage {
+
+	public static void main(String[] args) {
+		//System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		//System.setProperty("Webdriver.firefox.marionette", "D:\\FirefoxDriver\\Geckodriver.exe");
+		ChromeDriver driver= new ChromeDriver();			
+		//WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://www.target.com/");
+		
+		driver.manage().window().maximize();
+		
+
+	}
+
+}
